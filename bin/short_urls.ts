f5a5056baf5978192fd1d7a1b5ec6789077fda39
Master__Harvey <ianpadraicharvey@gmail.com>
2022-12-13 13:24:51 -0700

    Cert Manager + Cloudfront only work in us-east-1 with cdk I guess

--- bin/short_urls.ts
+++ bin/short_urls.ts
@@ -4,4 +4,4 @@ import * as cdk from 'aws-cdk-lib';
 import { ShortUrlsStack } from '../lib/short_urls-stack';
 
 const app = new cdk.App();
-new ShortUrlsStack(app, 'ShortUrlsStack', {});
+new ShortUrlsStack(app, 'ShortUrlsStack', { env: { region: "us-east-1" } });
