d42e58bfe0339119fab3406dcf01c1661afe15bf
Master__Harvey <ianpadraicharvey@gmail.com>
2023-01-05 15:16:32 -0700

    updated typescript version and stack desc

--- bin/short_urls.ts
+++ bin/short_urls.ts
@@ -4,4 +4,4 @@ import * as cdk from 'aws-cdk-lib';
 import { ShortUrlsStack } from '../lib/short_urls-stack';
 
 const app = new cdk.App();
-new ShortUrlsStack(app, 'ShortUrlsStack', { env: { region: "us-east-1" } });
+new ShortUrlsStack(app, 'ShortUrlsStack', { env: { region: "us-east-1" }, description: "S3 URL shortener" });
