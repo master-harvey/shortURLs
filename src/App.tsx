5788b17b23ea2928d33bf112700277fd68b2e38c
Master__Harvey <ianpadraicharvey@gmail.com>
2023-01-09 15:40:10 -0700

    global state

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import gitLogo from './github-mark-white.svg'
 import './App.css'
 
-import { AppBar, Toolbar, Box, IconButton, Typography, TextField, InputAdornment } from '@mui/material'
+import { AppBar, Toolbar, Box, IconButton, Typography, TextField, InputAdornment, Card, Button } from '@mui/material'
 
 import SearchIcon from '@mui/icons-material/Search';
 import Add from './Add';
 import Rem from './Rem';
 import List from './List';
 
+import { store } from "./store.js"
+import { useSnapshot } from "valtio";
+
+import { functionURL } from "./functionURL.txt"
+
 function App() {
+  const snap = useSnapshot(store)
+
   return (<>
-    <AppBar>
+    <AppBar sx={{ backgroundColor: '#fe3232' }}>
       <Toolbar>
-        <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', width: '100%' }}>
-          <Typography variant="h4">Manage shortURLs</Typography>
-          <TextField
-            variant="filled"
-            label="Search codes and destinations"
-            id="input-with-icon-textfield"
-            sx={{ flexGrow: 1, mx: 3 }}
+        <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', alignItems: 'center', width: '100%' }}>
+          <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+            <IconButton edge="start" color="inherit" aria-label="open drawer" sx={{ p: 0, my: 1, mx: 2 }}>
+              <a href="https://github.com/master-harvey/shortURLs" target="_blank">
+                <img src={gitLogo} className="Github logo" alt="Github logo" id="logo" />
+              </a>
+            </IconButton>
+            <Typography sx={{ mr: 2 }} variant="h4">Manage shortURLs</Typography>
+          </Box>
+          <TextField variant="filled" label="Search codes and destinations" id="input-with-icon-textfield" sx={{ flexGrow: 1 }}
+            value={snap.searchBar}
+            onChange={(e) => store.searchBar = e.target.value}
             InputProps={{
               startAdornment: (
                 <InputAdornment position="start">
@@ -27,20 +39,21 @@ function App() {
               ),
             }}
           />
-          <IconButton edge="start" color="inherit" aria-label="open drawer">
-            <a href="https://github.com/master-harvey/shortURLs" target="_blank">
-              <img src={gitLogo} className="Github logo" alt="Github logo" id="logo" />
-            </a>
-          </IconButton>
         </Box>
       </Toolbar>
     </AppBar >
 
     <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', alignItems: 'center' }}>
-        <Box sx={{ m: 2 }}><Add /></Box>
-        <Box sx={{ m: 2 }}><Rem /></Box>
-      </Box>
+      <Card sx={{ p: 2, m: 2 }}>
+        <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'space-around' }}>
+          <TextField type="password" variant='filled' label="PassKey" value={snap.passKey} onChange={(e) => store.passKey = e.target.value} id="input-with-icon-textfield" sx={{ width: '60%', mx: 2 }} />
+          <Button variant="outlined" sx={{ my: 1 }} onClick={() => console.log(store)}>Submit</Button>
+        </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'center' }}>
+          <Box sx={{ m: 1, flexGrow: 1 }}><Add /></Box>
+          <Box sx={{ m: 1, flexGrow: 1 }}><Rem /></Box>
+        </Box>
+      </Card>
       <List />
     </Box>
   </>)
--- src/Rem.tsx
+++ src/Rem.tsx
@@ -1,8 +1,13 @@
 //Form to remove a redirect
-import { Card, Typography } from '@mui/material'
+import { Card, Typography, TextField } from '@mui/material'
+
+import { store } from "./store.js"
+import { useSnapshot } from "valtio";
 
 export default function Rem() {
-    return <Card sx={{ p:2 }}>
+    const snap = useSnapshot(store)
+    return <Card sx={{ p: 2 }}>
         <Typography variant="h5">Remove a redirect</Typography>
+        <TextField variant='filled' label="Forward Code" id="remCode" sx={{ width: '100%' }} value={snap.remCode} onChange={(e) => store.remCode = e.target.value} />
     </Card>
 }
